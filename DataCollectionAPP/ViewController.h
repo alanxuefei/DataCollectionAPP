22bcf7ffaa0b92a50745e2d63be4fafeb83e4058
alanxuefei <alan.xuefei@googlemail.com>
2015-11-25 13:31:56 +0800

    Initial Commit

---
+++ DataCollectionAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataCollectionAPP
+//
+//  Created by NewUser on 25/11/15.
+//  Copyright © 2015 mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataCollectionAPP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DataCollectionAPP
+//
+//  Created by NewUser on 25/11/15.
+//  Copyright © 2015 mobile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
